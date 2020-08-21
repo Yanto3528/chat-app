ce676159f30628389f3d50872ec334af414918f6
Yanto Lee <yanto3528@gmail.com>
2020-08-21 13:57:24 +0700

    initialize firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB3sqHa7dh7hWzRw65wK4FmdNLSrmVRV7w",
+  authDomain: "marioplan-78f63.firebaseapp.com",
+  databaseURL: "https://marioplan-78f63.firebaseio.com",
+  projectId: "marioplan-78f63",
+  storageBucket: "marioplan-78f63.appspot.com",
+  messagingSenderId: "16686255660",
+  appId: "1:16686255660:web:0a28e2ad0aa00a1ecf8b89",
+  measurementId: "G-WD8NJX5V3S",
+};
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
