c40df5d236e3992f6b251aec7541810b2c317b2d
Yanto Lee <yanto3528@gmail.com>
2020-08-21 18:39:53 +0700

    started building dashboard; added sign out button

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Drawer, Button } from "rsuite";
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
+import { useProfileContext } from "../../context/profile.context";
+
+const Dashboard = ({ onSignOut }) => {
+  const { profile } = useProfileContext();
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title></Drawer.Title>
+      </Drawer.Header>
+
+      <Drawer.Body>
+        <h3>Hey, {profile.name}</h3>
+      </Drawer.Body>
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>
+          Sign out
+        </Button>
+      </Drawer.Footer>
+    </>
+  );
 };
 
 export default Dashboard;
